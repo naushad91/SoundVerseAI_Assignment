0d71919c543456cb63af59c8dffd3773a6f0e18e
Naushad Ali Khan <naushadkhank426@gmail.com>
2026-05-07 23:51:00 +0530

    Initial Commit

---
+++ SoundverseAI/SoundverseAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundverseAIApp.swift
+//  SoundverseAI
+//
+//  Created by Naushad Ali Khan on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundverseAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
